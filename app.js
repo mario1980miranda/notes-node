1963dd79cc1936c92204421db5843e0d975c0c36
mario_chan15 <mario.luiz.miranda@gmail.com>
2018-04-16 11:58:18 -0300

    S03-C14 - Parsing Args w/ yargs

--- app.js
+++ app.js
@@ -2,23 +2,30 @@ console.log('Starting App');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
-var command = process.argv[2];
-
-console.log(process.argv);
-
-console.log('Command: ', command);
+const argv = yargs.argv;
+var command = argv._[0];
 
 if (command === 'add') {
-    console.log('Adding new note');
+    
+    notes.addNote(argv.title, argv.body);
+
 } else if (command === 'list') {
-    console.log('Listing all notes');
+
+    notes.getAll();
+
 } else if (command === 'read') {
-    console.log('Reading note');
+    
+    notes.getNote(argv.title);
+
 } else if (command == 'remove') {
-    console.log('Removing note');
+    
+    notes.removeNote(argv.title);
+
 } else {
+
     console.log('Command not recognised');
 }
--- notes.js
+++ notes.js
@@ -1,1 +1,29 @@
 console.log('Starting note.js');
+
+
+var getAll = () => {
+
+    console.log('Getting all notes');
+};
+
+var addNote = (title, body) => {
+
+    console.log('Adding note', title, body);
+};
+
+var getNote = (title) => {
+
+    console.log('Getting note by title: ', title);
+}
+
+var removeNote = (title) => {
+
+    console.log('Removing note by title: ', title);
+}
+
+module.exports = {
+    addNote,
+    getAll,
+    getNote,
+    removeNote
+};
